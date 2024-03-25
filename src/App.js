0c36a3956b2ee2f7b23b54ed2b10e2e366bd65da
Guilherme Moretti <68204019+GuirraMoretti@users.noreply.github.com>
2024-03-24 21:01:10 -0300

    WEB2024
    dasda

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import ComponenteAvo from "./Atividade02/Questão02/ComponenteAvo";
+import FunctionContextA from "./Atividade02/Questão01/FunctionContextA";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>
+    <h4>Questão 01</h4>
+      <FunctionContextA />
+      <h4>Questão 02</h4>
+      <ComponenteAvo />
+      <h4>A questão 3 ja está resolvida dentro da questão 2</h4>
+  </div>
 }
 
 export default App;
